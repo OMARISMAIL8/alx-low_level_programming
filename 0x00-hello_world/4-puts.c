4210f98333e71c516684fa130214086456fe32bb
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-15 17:46:53 -0700

    updated file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-int main (void)
 /**
- * the first C coding*/
+ * main - Entry point
+ * Description: A C is perfect language
+ * Return: Alawys 0 (success)
+*/
+int main(void)
 
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
