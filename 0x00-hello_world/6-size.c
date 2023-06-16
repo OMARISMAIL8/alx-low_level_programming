97610e4f05b8d05885025a767f9df7c2c13eceb5
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-15 18:16:37 -0700

    updated file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: A C is perfect language
+ * Return: Alawys 0 (success)
+*/
+int main(void)
+{
+	printf("Size of a char: %ld byete(s)\n", sizeof(char));
+	printf("Size of an int: %ld byete(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byete(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byete(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byete(s)\n", sizeof(float));
+
+	return (0);
+}
