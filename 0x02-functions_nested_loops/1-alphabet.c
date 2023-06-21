c979ac4e0e52c9e553549f1a411238f17c1d8fd4
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-21 04:03:07 -0700

    updated file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * print_alphabet - prints the alphabe, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+
+	int ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	_putchar('\n');
+
+return (0);
+}
