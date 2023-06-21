90fd8e1bb02e7318cc9207bd0acdc5b654b33909
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-21 05:59:29 -0700

    updated file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,12 +18,12 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	if (n == '0')
+	else if (n == '0')
 	{
 		putchar(48);
 		return (0);
 	}
-	if (n < '0')
+	else if (n < '0')
 	{
 		putchar(45)
 		return (-1);
