f111821e298dc7d6da6a2a36835e1ef951b391f0
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-23 09:52:19 -0700

    updated file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int isupper()
+{
+	if(c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
