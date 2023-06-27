cfbb03d9e6fbb117956c8efe77395f9ca32ddb79
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-06-27 14:26:47 -0700

    updated file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+include"main.h"
+/**
+ *_strcpy- copies string
+ *@dest:checked
+ *@src: checked
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	a = 0;
+
+	while (src[a])
+	{
+		src[a] = dest[a]
+		i++;
+	}
+	return (dest);
+}
