481c0a40b9e000698db5154f088c9992a7ceb2be
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-07 12:46:00 -0700

    updated file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j = 0;
+	int i, j = 1;
 
 	if (argc != 3)
 	{
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
-			j = atoi(argv[i]) * atoi(argv[i]);
+			j *= atoi(argv[i]);
 		}
 		printf("%d\n", j);
 	}
