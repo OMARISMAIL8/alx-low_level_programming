dd5171e85fb01216195d6b6be01bd80270202cc7
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-07 11:14:02 -0700

    updated file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
