90be4fb8abb0cf36a95326f7836ec718e3bc7660
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-07 12:16:02 -0700

    updated file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int omar;
+
+	if (omar == 3)
+	{
+		omar = atoi(argv[1]) * atoi(argv[2])
+		printf("%d\n", omar);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
