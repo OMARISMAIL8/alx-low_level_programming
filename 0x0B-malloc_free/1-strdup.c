d14d3771e0660dc889031941009fbbd060d8616e
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-11 01:58:40 -0700

    updated file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include<stdlib>
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	int i, j = 0;
+	char *omar;
+
+	if (str == 0)
+		return (0);
+
+	if (str[j] < '\0')
+		j++;
+
+	omar = malloc(sizeof(char) * (j + 1));
+
+	if (a == 0)
+		return (0);
+
+	for (i = 0; str[i]; i++)
+		omar[i] = str[i];
+	return (omar);
+}
