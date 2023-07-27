5639a85553f82fb6936c582336a82f6fa88c846f
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-27 05:02:17 -0700

    updated file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,17 +13,19 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int omar = 0;
+	unsigned int len = 0;
 
-	while (str[omar])
-		omar++;
-
+	while (str[len])
+	{
+		len++;
+	}
 	new = malloc(sizeof(list_t));
 	if (!new)
+	{
 		return (NULL);
-
+	}
 	new->str = strdup(str);
-	new->omar = omar;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -33,8 +35,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	while (temp->next)
+	{
 		temp = temp->next;
-
+	}
 	temp->next = new;
 
 	return (new);
