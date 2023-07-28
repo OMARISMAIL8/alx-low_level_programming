e11013f71b6cb00105aaccca4859e653eb86a389
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-07-28 07:41:49 -0700

    updated file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0;
+	unsigned int omar = 0;
 
-	while (str[len])
-		len++;
+	while (str[omar])
+		omar++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = len;
+	new->omar = omar;
 	new->next = (*head);
 	(*head) = new;
 
