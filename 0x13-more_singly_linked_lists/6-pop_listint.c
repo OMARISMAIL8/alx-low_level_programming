80ebef2e6f34720f51f725da7cee6eeab5de35a9
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-01 09:23:48 -0700

    updated file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
+ * pop_listint - deletes the head node
  * @head: pointer to the first element in the linked list
  *
  * Return: the data inside the elements that was deleted,
@@ -20,5 +20,5 @@ int pop_listint(listint_t **head)
 	free(*head);
 	*head = omar;
 
-	return (num);
+	return (ismail);
 }
