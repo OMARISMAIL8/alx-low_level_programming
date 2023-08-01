fe352370853b25d94ffbdaafca1a22d721145c17
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-01 09:14:09 -0700

    updated file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * head: pointer to the listint_t list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *omar;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		omar = (*head)->next;
+		free(*head);
+		*head = omar;
+	}
+
+	*head = NULL;
+}
