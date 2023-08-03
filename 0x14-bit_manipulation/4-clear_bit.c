70d29973650711b9a3d9f4c3a56bfdb449acbdb8
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-03 13:16:45 -0700

    updated file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * clear_bit - set the value of a bit to 0
+ * @n: pointer to decimal number
+ * @index: index position
+ * Return: 1 if it worked, -1 if it's error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int omar;
+	unsigned int ismail;
+
+	if (index > 64)
+		return (-1);
+	ismail = index;
+	for (omar = 1; hold > 0; omar *= 2, ismail--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= omar;
+
+	return (1);
+}
