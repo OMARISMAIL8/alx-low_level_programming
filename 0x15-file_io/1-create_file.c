59963faf9249c1abc5a2e58d035edf7bf1ba9036
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-07 07:53:14 -0700

    updated file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: A pointer to a string to write.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+        int omar, ismail, o = 0;
+
+        if (filename == NULL)
+                return (-1);
+
+        if (text_content != NULL)
+        {
+                for (o = 0; text_content[o];)
+                        o++;
+        }
+
+        omar = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        ismail = write(omar, text_content, len);
+
+        if (omar == -1 || ismail == -1)
+                return (-1);
+
+        close(omar);
+
+        return (1);
+}
