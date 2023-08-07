cafa38e43d5ea03a0d975c389ffeb1c432bfb04c
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-07 07:56:50 -0700

    updated file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int omar, ismail, o = 0;
+	int omar, ismail, o = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (o = 0; text_content[o];)
-                        o++;
-        }
+	if (text_content != NULL)
+	{
+		for (o = 0; text_content[o];)
+			o++;
+	}
 
-        omar = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        ismail = write(omar, text_content, o);
+	omar = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	ismail = write(omar, text_content, o);
 
-        if (omar == -1 || ismail == -1)
-                return (-1);
+	if (omar == -1 || ismail == -1)
+		return (-1);
 
-        close(omar);
+	close(omar);
 
-        return (1);
+	return (1);
 }
