be687a4592d8247d43de062d29cd010eca1952aa
OMARISMAIL8 <omarismail1982000@gmail.com>
2023-08-07 09:25:34 -0700

    updated file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: A pointer to the name of file
+ * @text_content: The string to add
  *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ * Return: If the function fails or filename is NULL - -1
+ *         If the file does not exist the user lacks write permissions - -1
+ *         Otherwise - 1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	omar = open(filename, O_WRONLY | O_APPEND);
 	ismail = write(omar, text_content, o);
 
-	if (omar == -1 || w == -1)
+	if (omar == -1 || ismail == -1)
 		return (-1);
 
 	close(omar);
